0abd0fe1182ecb835d34e4b3de12fd638a573bb8
AB5123 <ab5123@student.jamk.fi>
2022-01-12 13:03:42 +0200

    Added RPS winner check

--- src/helpers.js
+++ src/helpers.js
@@ -11,4 +11,34 @@ module.exports = {
     };
     return new Date(date).toLocaleDateString(locale, options);
   },
+  // Check for RPS winner
+  checkWinner(a, b) {
+    if (a === b) {
+      return 'TIE';
+    }
+    // PAPER
+    if (a === 'PAPER') {
+      if (b === 'ROCK') {
+        return 'A'
+      } else if (b === 'SCISSORS') {
+        return 'B'
+      }
+    }
+    // ROCK
+    if (a === 'ROCK') {
+      if (b === 'SCISSORS') {
+        return 'A'
+      } else if (b === 'PAPER') {
+        return 'B'
+      }
+    }
+    // SCISSORS
+    if (a === 'SCISSORS') {
+      if (b === 'PAPER') {
+        return 'A'
+      } else if (b === 'ROCK') {
+        return 'B'
+      }
+    }
+  },
 };
