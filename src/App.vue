85b98d6a38acd11a5e8f0472dbdb26c841d8c090
Tapio Ekman <ab5123@student.jamk.fi>
2022-01-14 11:42:00 +0200

    Added a header for app

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <v-app>
     <v-container>
+      <h2 class="text-center">ROCK, PAPER, SCISSORS GAMES</h2>
       <v-row>
         <v-col><GameHistory /></v-col>
         <v-col><LiveData /></v-col>
